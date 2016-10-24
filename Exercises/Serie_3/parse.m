3c6e0c18742bdfeff473dccb77df8a7f6cea47bc
LaurentHayez <LaurentHayez@users.noreply.github.com>
2016-10-24 16:05:19 +0200

    Finished serie 3

--- Exercises/Serie_3/parse.m
+++ Exercises/Serie_3/parse.m
@@ -11,9 +11,16 @@ function [n c] = parse( s )
 % Assumptions:
 %   - The user does not enter expression such as 3X1 + 6X1, but directly
 %     9X1.
-%   - x1 and X1 are different variables
+%   - The user enters only the same type of variables (ie only X or only y, etc.)
+%   - The variables are entered in ascending order
 %   - the constraint is the only number on the right hand side, i.e.
 %     x1 + x2 > x3 - 3 is not valid, but x1 + x2 - x3 > -3 is.
+%
+% Note: This exercise was done with regular expressions. I chose to use
+%       regexp just to challenge myself. With a bit more work, some of 
+%       the above conditions could be relaxed. But the code is already
+%       long and obscure due to the use of regexp, so I chose to have
+%       stronger assumptions and a simpler code.
 
     
 %% String preprocessing    
@@ -35,18 +42,47 @@ end
 s = new_s;
 
 %dynamic regex to replace all expresions such as x1 by 1x1
-s = regexprep(s, '^[a-zA-Z]\w*|(?<=(\s[+-])*[^0-9\*])[a-zA-Z]\w*', '1$0');
+s = regexprep(s, '^[a-zA-Z]\w*|(?<=(+|-))([a-zA-Z]\w*)', '1$0');
 
 % regex to get the variables
 var = regexp(s, '[a-zA-Z]\w*', 'match');
-n = length(var);
-
-%% TODO: make regexp to get the different variables and then create the coefficients accorgingly
+% get last variable
+last_var = var{length(var)};
+% get last variable index
+last_var_index = str2num(cell2mat(regexp(last_var, '(?<=[a-zA-Z]+)[0-9]*', 'match')));
+%last_var_index = str2num(last_var_index);
+% get variable name
+var_name = cell2mat(regexp(last_var, '[a-zA-Z]+(?=[0-9]*)', 'match'));
 
 % regex to get the coefficients corresponding to the variables
-c = regexp(s, '-?[0-9]+(?=[a-zA-Z]\w*)', 'match');
+c = regexp(s, '(-?[0-9]+)(?=[a-zA-Z]+\w*)', 'match');
 % Getting the last coefficients corresponding to the constraint
 last_const = regexp(s, '(?<=[<>=]+)-?[0-9]+', 'match');
 c = [c last_const];
 % c is a cell with string inside => convert to numbers
 c = cellfun(@str2num, c);
+
+% add coefficient 0 to missing variables
+new_coeffs = [];
+for i=1:last_var_index
+    [tf, idx] = ismember([var_name num2str(i)], var);
+    if tf
+        new_coeffs = [new_coeffs c(idx)];
+    else
+        new_coeffs = [new_coeffs 0];
+    end
+end
+
+assert(length(c) == length(var) + 1, ['The number of coefficients is not equal to the number of variables. ' ...
+                    'Something went wrong.']);
+% Adding the constraint to the list of coefficients
+new_coeffs = [new_coeffs c(length(c))];
+
+c = new_coeffs;
+n = length(c) - 1;
+
+%% Examples to test:
+% expression = '4X1 - 5X2 + X3 < 12'; [n c] = parse( expression )
+% expression = 'X1 - 7X3 + 2X5 > 3'; [n c] = parse( expression )
+% expression = '-5y1 + 2y4 -3*y6 < 9'; [n c] = parse( expression )
+% expression = '- 1050 myAwesomeVariable1 + 1291myAwesomeVariable4 + 12*myAwesomeVariable7 > 25'; [n c] = parse( expression )
