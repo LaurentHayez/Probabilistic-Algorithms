155ead8b48b213c1672713e65ceb45c3bf89dc25
LaurentHayez <LaurentHayez@users.noreply.github.com>
2016-12-27 09:54:35 +0100

    Start project: generate distances between cities

---
+++ Project/PA_project_init_TSP.m
@@ -0,0 +1,30 @@
+% Author: Laurent Hayez
+% Date: 27.12.2016
+% File: initialization of TSP problems
+
+clear all;
+addpath(genpath('./'));
+
+disp('Initializing TSP problem...');
+
+% generate the distance matrix for the TSP problem
+cities = 'TSP_test.txt';
+
+file = fopen(cities, 'r');
+
+disp('Reading file...')
+
+coordinate = fscanf(file, '%f', [3, Inf]);
+
+fclose(file);
+
+citiesCoordinate = coordinate(2:3, :);
+
+disp('Computing distances between cities...');
+
+citiesDistances = computeDistanceMatrix(citiesCoordinate);
+
+% number of times to run the algorithms
+maxRun = 30;
+
+disp('End of initialization');
