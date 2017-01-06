8907e72b0be88366f75d416f24a92b92c993b69b
LaurentHayez <LaurentHayez@users.noreply.github.com>
2017-01-06 17:39:17 +0100

    Modified shortestEdge so that it is randomized

--- Project/shortestEdge.m
+++ Project/shortestEdge.m
@@ -28,7 +28,7 @@ end
 
 %disp([edges(:).len]);
 
-[sortedEdges, permutationMatrix] = sort([edges(:).len]);
+[sortedLengths, permutationMatrix] = sort([edges(:).len]);
 
 % sort edges to keep structure
 edges = edges(permutationMatrix);
@@ -41,6 +41,20 @@ nodeDegree = zeros(1, n);
 % next edge in the sorted list of edges
 nextEdgeCounter = 1;
 
+% randomly permute the edges that have the same length
+% for this, we consider each distance, find the indexes for which the edges
+% have the same length, and randomly permute those indexes.
+% (if we don't do this the algorithm is deterministic and always yields the
+% same result)
+uniqueLengths = unique(sortedLengths);
+for i = 1:length(uniqueLengths)
+    % find indexes where the edges have the same length
+    idx = find(sortedLengths == uniqueLengths(i));
+    % randomly permute them
+    rp = randperm(length(idx));
+    edges(idx) = edges(idx(rp));
+end
+
 for i=1:n
     % select next shortest edge
     edgeIsValid = false;
@@ -50,6 +64,7 @@ for i=1:n
             break;
         end
         nextEdge = edges(nextEdgeCounter);
+        
         edgeIsValid = checkConditions(nextEdge, nodeDegree, ...
             adjacencyMatrix);
         nextEdgeCounter = nextEdgeCounter + 1;
